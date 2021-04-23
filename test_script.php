06de2da5282555e6fd81c78c4043f354f89e15f6
Nick Zorych <unix-admin@users.noreply.github.com>
2021-04-23 11:51:57 +0300

    changes
    maybe refactor

--- test_script.php
+++ test_script.php
@@ -4,26 +4,8 @@ declare(strict_types=1);
 if(empty($argv[1]) || empty($argv[2]) || !is_numeric($argv[1]) || !is_numeric($argv[2]) || $argv[1] < 2 || $argv[2] < 2 )
   exit('Неверные параметры!'.PHP_EOL);
 
-$matrix = calculateMatrixSize((int)$argv[1],(int)$argv[2]);
-echo 'В матрице прямоугольников: '.rectangles(++$matrix['cols'],2)*rectangles(++$matrix['rows'],2).PHP_EOL;
+echo 'В матрице прямоугольников: '.rectangles((int)$argv[1],2)*rectangles((int)$argv[2],2).PHP_EOL;
 
-
-/**
- * Вычисляет размер матрицы по количеству точек
- * @param int $x
- * @param int $y
- * @return array
- */
-function calculateMatrixSize(int $x, int $y){
-  $cols = 0;
-  $rows = 0;
-  for ($i=1; $i<$x; $i++){
-    for($j = 1; $j<$y; $j++)
-      $rows++;
-    $cols++;
-  }
-  return['cols'=>$cols,'rows'=>$rows/$cols];
-}
 
 /**
  * Вычисляет факториал числа
